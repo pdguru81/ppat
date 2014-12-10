2600db1d7641f55693a7e6f4cfdfda9446674d07
Philip Abel <philipa@mit.edu>
2014-12-10 04:47:42 -0500

    added a better layout

--- javascripts/teamkate.js
+++ javascripts/teamkate.js
@@ -3,8 +3,12 @@
 var comment = 'Our team was comprised of four members. We worked on this project all semester and multiple prototypes for the project. Checkout their comments'+
  					   +'about this class below';
 
-var projectdetails =' We completed this project and it was interesting!.... more to come';
+var projectdetails =' Welcome! We are team Kate from 6.811- Principles and Practice of Assistive Technology.'+
+ " We worked with our client, Kate, in cambridge  to create a water resistant, noise blocking cover for her cochlear implant!" +
+ ' This incredibly fun project took about eight weeks and we use this site to document the work that we have done so far. Feel free to look at the docs!';
 
+
+//instructions on how to use instamorph
 var instamorph='<ol style="type:1">'+
 "Making instamorph ("+"<a href='http://www.instamorph.com/'>instamorph link</a>"+ ") coil covers:"+
  '<li>Pour instamorph pellets in hot water</li>'+
@@ -20,7 +24,7 @@ var instamorph='<ol style="type:1">'+
 '<li>Trim the edges of the fabric to match the shape of the instamorph piece.</li>'+
 '</ol>';
 
-
+// Instructions on how to use sugru
 var sugru ='<ol style="type:1">'+
 "Making sugru (<a href='http://sugru.com/'>Sugru link</a>) sound blockers: "+
 "<li> To protect the cochlear implant and to allow the sugru to be easily removed from the cochlear implant, the first step is to wrap the cochlear implant in saran wrap. The layer should be as thin as possible while also being fitted enough so that it doesn't easily move around. It should also be wrapped so that it can be slipped off the front of the cochlear implant easily. </li>"+
@@ -31,11 +35,49 @@ var sugru ='<ol style="type:1">'+
 "<li> The next day, the sugru piece should be dried and solid. You will be able to peel off the saran wrap from the inside of the sugru piece.</li>"+
 "<li> In order to add more grip to the sugru piece so that it wouldnt easily fall off of the cochlear implant, we used dycem (http://www.dycem-ns.com//node/250).</li>"+
 "We cut very small pieces of dycem and used plastic surgery glue to attach them to the inside front and pack of the sugru sound blocker piece.";
-'</ol>'+
-$(document).ready(function(){
+'</ol>';
 
+
+
+
+var loadhaat = function(){
+		 $("#haat-summary").load("HAATForWeb/HAATForWeb.html",function(response){
+		 		$('#haat-summary').html(response);
+		 }); 
+	  }
+
+
+var loadtestAndResults = function(){
+	$("#testsAndResults").load("testsAndResults/testsAndResults.html",function(response){
+		 		$('#testsAndResults').html(response);
+		 }); 
+}
+
+
+
+
+$(document).ready(function(){
+  	//load the haat summary page
+  	//loadtestAndResults();
+  	//loadhaat();
 	$('#project-details').html(projectdetails);
 	$('#teamcomment').html(comment);
-	$('#instamorph').html(instamorph);
-	$('#sugru').html(sugru);
+	//$('#instamorph').html(instamorph);
+	//$('#sugru').html(sugru);
+
+	$('#success').click(function() {
+		//loadtestAndResults();
+
+	});
+
+	// prevent panels from jumping
+	$('.panel-title a').on('click',function(e){
+		var panel =$(this).parents('.panel-title').parents('.panel-heading').parents('.panel').children('.panel-collapse');
+		console.log("here");
+		e.preventDefault();
+	    
+	});
+
+
+
 });
